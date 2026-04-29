fee9d024a4ba36e413eaa89878881f56b223a16d
furkanyallic <yallicfurkann@gmail.com>
2026-04-29 13:02:27 +0300

    Bottom navigation bar added

--- app/src/main/java/com/example/vetroid/MainActivity.kt
+++ app/src/main/java/com/example/vetroid/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,40 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Bottom Navigation View setup
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragment_container, HomeFragment())
+                        commit()
+                    }
+                    true
+                }
+                R.id.nav_macros -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragment_container, MacrosFragment())
+                        commit()
+                    }
+                    true
+                }
+                R.id.nav_settings -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragment_container, SettingsFragment())
+                        commit()
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Set default selection to home
+        if (savedInstanceState == null) {
+            bottomNav.selectedItemId = R.id.nav_home
         }
     }
 }
